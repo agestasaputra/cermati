46c7ec89c97712e418bf28cc5acce1187863df67
Agesta Saputra <atsegasaputra@outlook.com>
2021-10-17 13:54:05 +0700

    Updates

---
+++ precache-manifest.86cf21b28cf7c0f2cb8e8b62242691df.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ff87caff98088da0893b60a81a1d3a",
+    "url": "/cermati/index.html"
+  },
+  {
+    "revision": "1fc0b5ff72e5b15404e6",
+    "url": "/cermati/static/css/2.d2ed95ac.chunk.css"
+  },
+  {
+    "revision": "e1ad224eb158654c00da",
+    "url": "/cermati/static/css/main.cf7c6c61.chunk.css"
+  },
+  {
+    "revision": "1fc0b5ff72e5b15404e6",
+    "url": "/cermati/static/js/2.be9a858b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/cermati/static/js/2.be9a858b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ad224eb158654c00da",
+    "url": "/cermati/static/js/main.a9858caf.chunk.js"
+  },
+  {
+    "revision": "534af14638848c6b2ddc",
+    "url": "/cermati/static/js/runtime-main.b223b11e.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/cermati/static/media/banner.a5a74b4f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cermati/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cermati/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cermati/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cermati/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cermati/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
